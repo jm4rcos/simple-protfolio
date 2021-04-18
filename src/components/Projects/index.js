9e8ca97d9daee39d5339ef5c53bef92baaaa40c0
jm4rcos <joaomarcosmacimiro@gmail.com>
2021-04-18 20:20:46 -0300

    update

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,19 +1,35 @@
+import covidtrackerImg from './images/development/covid.png'
+import weatherApp from './images/development/weather.png'
+
+import coding from '../../assets/coding.svg'
+
 export const frontEnd = [
     {
         title:'COVID19 TRACKER',
-        desc:'COVID19 Tracker from every Country',
-        img:''
+        desc:'COVID19 Tracker from every Country.',
+        img: covidtrackerImg,
+
+        moreInfo:'Created wicth ReactJS and ChartJS',
+        link:'https://cov19-tracker-reactjs.netlify.app/'
     },
     {
         title:'Weather App',
         desc:'City Weather App',
-        img:''
+        img: weatherApp,
+
+        moreInfo:'Created with ReactJS and Axios',
+        link:'https://kind-weather-app.netlify.app/'
     },
     
 ]
 
 export const design = []
 
-export const illustration = []
+export const illustration = [
+    {
+        img: {coding},
+        moreInfo:'Created with Adobe Illustrator'
+    },
+]
 
 
--- src/components/utils/utils.jsx
+++ src/components/utils/utils.jsx
@@ -1,50 +1,58 @@
 import React from 'react'
 
 import styles from './utils.module.css'
-import {frontEnd} from '../Projects'
+import {frontEnd, design, illustration} from '../Projects'
 
 export const FrontEndWorks =() =>{
-
-/*     const [frontEndProject, setFrontEndProject] = React.useState(data) */
 
     console.log(frontEnd);
     return(
         <div className={styles.container}>
-            {frontEnd.map(project => {
-                const {title, desc, img} = project
-                return(
-                    <div className={styles.frontEndWork} style={{color:'#fff'}}>
-                        <h3 className={styles.title}>{title}</h3>
-                        <div className={styles.desc}>{desc}</div>
+            {frontEnd.map((project, id) => {
+                const {title, desc, img, moreInfo, link} = project
+                return(<div key={id} style={{display: 'flex'}}>
+                        <div className={styles.frontEndWork}>
+                            <img src={img} draggable={false} className={styles.img}/>
+                        </div>
+                        <div style={{width: '220px',color:'#656565',display: 'block', background: 'none', marginRight:'40px', position:'relative'}}>
+                            <h3 className={styles.title}>{title}</h3>
+                            <div className={styles.desc}>{desc}</div>
+                            <div style={{bottom:'0px', position:'absolute'}}>
+                                <p>{moreInfo}</p>
+                                <a href={link} style={{fontWeight:'bold',fontSize:'0.6em', outline:'none', textDecoration:'none', background:'lightgray', borderRadius: '5px', padding: '5px'}}>{link}</a>
+                            </div>
+                        </div>
                     </div>
                 )
             })}
-            {/* <div className={styles.frontEndWork}>FrontEnd</div>
-            <div className={styles.frontEndWork}>FrontEnd</div>
-            <div className={styles.frontEndWork}>FrontEnd</div>
-            <div className={styles.frontEndWork}>FrontEnd</div> */}
         </div>
     )
 }
 
 export const DesignWorks = () => {
     return(
-        <div className={styles.container}>
-            <div className={styles.designWork}>Design</div>
-            <div className={styles.designWork}>Design</div>
-            <div className={styles.designWork}>Design</div>
-            <div className={styles.designWork}>Design</div>
-        </div>
+        <div>Design</div>
     )
 }
 
 export const IllustrationWorks = () => {
     return(
         <div className={styles.container}>
-            <div className={styles.illustrationWork}>Illustration</div>
-            <div className={styles.illustrationWork}>Illustration</div>
-            <div className={styles.illustrationWork}>Illustration</div>
-            <div className={styles.illustrationWork}>Illustration</div>
+            {illustration.map((project, id) => {
+                const {img, moreInfo} = project
+                return(<div key={id} style={{display: 'flex'}}>
+                        <div className={styles.illustrationWork}>
+                            <img src={img} draggable={false} className={styles.img}/>
+                            <h1>Coding Boy</h1>
+                        </div>
+                        <div style={{width: '220px',color:'#656565',display: 'block', background: 'none', marginRight:'40px', position:'relative'}}>
+                            <div style={{bottom:'0px', position:'absolute'}}>
+                            <p>{moreInfo}</p>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })}
         </div>
-    )
+        )
 }
